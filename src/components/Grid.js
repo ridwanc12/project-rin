a51513d0ccf2d18bf359c680eaad0015357e6c3c
Ridwan Chowdhury <rchowdhury1212@gmail.com>
2022-06-26 22:54:59 -0700

    Added a 'no results found' render

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -6,10 +6,18 @@ import SearchResults from "./SearchResults";
 function Grid({ query, numResults }) {
   const showResults = SearchResults(query, numResults);
 
-  if (!Array.isArray(showResults) || !showResults.length) {
+  if (showResults == null) {
     return (
       <div className="loading">
         <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (!showResults.length) {
+    return (
+      <div className="no-results">
+        <h1>No results found</h1>
       </div>
     );
   }
